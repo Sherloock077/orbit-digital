c2b6d7a864b4ecd116399e0aedcd6c6254a7e8cf
Sherloock077 <magjan090@gmail.com>
2026-05-16 19:02:31 +0500

    fix: финальный адаптив для телефона (подзаголовок 18px)

--- src/App.jsx
+++ src/App.jsx
@@ -377,8 +377,8 @@ function App() {
               </span>
             </h1>
 
-            <p className="text-base sm:text-lg md:text-2xl text-gray-400 max-w-xl md:max-w-3xl mx-auto font-light tracking-[0.15em] sm:tracking-[0.2em] md:tracking-[0.4em] uppercase leading-[1.6] border-t border-b border-white/10 py-4 sm:py-5 md:py-6 bg-black/20 backdrop-blur-sm px-2">
-    // Мы создаем технологии на стыке <br /> физической безопасности и цифрового интеллекта.
+            <p className="text-base sm:text-lg md:text-2xl text-gray-400 max-w-full md:max-w-3xl mx-auto font-light tracking-[0.05em] sm:tracking-[0.1em] md:tracking-[0.4em] uppercase leading-[1.6] border-t border-b border-white/10 py-4 sm:py-5 md:py-6 bg-black/20 backdrop-blur-sm px-4 sm:px-6">
+  // Мы создаем технологии на стыке <br /> физической безопасности и цифрового интеллекта.
             </p>
           </motion.div>
 
